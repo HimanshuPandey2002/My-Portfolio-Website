79400eacc8aa9f21c12d4ed4efdc80c6d3e6be87
Himanshu Pandey <hp200229@gmail.com>
2022-04-06 00:56:34 +0530

    Footer Done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./css/footer.css";
+import "./css/Footer.css";
 import footerCurve from "../Assets/footer-curve.svg";
 import footerCurveLight from "../Assets/footer-curve-light.svg";
 import github from "../Assets/social/github.svg";
@@ -8,10 +8,55 @@ import linkedin from "../Assets/social/linkedin.svg";
 import leetcode from "../Assets/social/github.svg";
 import instagram from "../Assets/social/instagram.svg";
 
-function Footer() {
+function Footer({ theme }) {
   return (
     <footer>
-      <div className="footer">footer</div>
+      <img
+        className="footer-curve"
+        src={theme.name === "dark" ? footerCurve : footerCurveLight}
+        alt=""
+      />
+      <div className="footer">
+        <div className="footer-head">Feel free to contact on social media.</div>
+        <div className="social">
+          <a
+            href="https://github.com/HimanshuPandey2002"
+            className="social-link"
+            target="blank"
+          >
+            <img src={github} alt="" className="social-icon" />
+          </a>
+          <a
+            href="https://twitter.com/himanshu_295"
+            className="social-link"
+            target="blank"
+          >
+            <img src={twitter} alt="" className="social-icon" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/himanshupandey29/"
+            className="social-link"
+            target="blank"
+          >
+            <img src={linkedin} alt="" className="social-icon" />
+          </a>
+          <a
+            href="https://leetcode.com/schafermike960/"
+            className="social-link"
+            target="blank"
+          >
+            <img src={leetcode} alt="" className="social-icon" />
+          </a>
+          <a
+            href="https://www.instagram.com/himanshupandey142/"
+            className="social-link"
+            target="blank"
+          >
+            <img src={instagram} alt="" className="social-icon" />
+          </a>
+        </div>
+        <div className="btm">Designed and Developed with ❤️ by Himanshu</div>
+      </div>
     </footer>
   );
 }
