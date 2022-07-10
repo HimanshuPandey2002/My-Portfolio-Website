e19877211fd0cbd04f3ee62adca3bec9513d3450
Himanshu Pandey <hp200229@gmail.com>
2022-07-10 09:07:12 +0530

    remove bugs

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
 import "./css/Projects.css";
-import projectImage from "../Assets/protostream.png";
+import projectImage1 from "../Assets/protostream.png";
+import projectImage2 from "../Assets/moviemania.png";
+import projectImage3 from "../Assets/periodictable.png";
 
 function Projects({ theme }) {
   const projects = [
     {
       title: "Protostream",
       desc: "A native-web app portal with multi-user interface, to faciliate a unique platform for students / startup fellows / entrepreneurs / venture capalist / angel investors / mentors / institutional head / funding organization.",
-      src: "../assets/protostream.png",
+      src: projectImage1,
+      // src: "../assets/protostream.png",
       live: "https://protostream.in/",
       code: "https://github.com/PROTOSTREAM",
+    },
+    {
+      title: "MovieMania",
+      desc: "A Web application build using react where you can find everyting about your favourite movies and TV shows. It also shows currently Trending & popular movies in the market.",
+      src: projectImage2,
+      live: "https://github.com/HimanshuPandey2002/GetMovies",
+      code: "https://github.com/HimanshuPandey2002/GetMovies",
+    },
+    {
+      title: "Periodic table",
+      desc: "A periodic table application created using React which shows information about each chemical element. Users can also filter elements by their metallic properties.",
+      src: projectImage3,
+      live: "https://github.com/HimanshuPandey2002/Periodic-Table/",
+      code: "https://github.com/HimanshuPandey2002/Periodic-Table://github.com/PROTOSTREAM",
     },
   ];
 
@@ -30,7 +47,7 @@ function Projects({ theme }) {
             desc={project.desc}
             live={project.live}
             code={project.code}
-            image={projectImage}
+            image={project.src}
             theme={theme}
           />
         ))}
